1b061b08cdd3924eb322d54d150eb4b36d98339f
Paul-Deepak <pauldeepak2003@gmail.com>
2023-10-31 12:48:24 +0530

    final

--- Practice/Problems/ConvertJsonToJavaProblem/ConvertJsonToJava.java
+++ Practice/Problems/ConvertJsonToJavaProblem/ConvertJsonToJava.java
@@ -4,21 +4,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.lang.reflect.*;
-import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class ConvertJsonToJava {
     private int age;
     private String name;
-    private String[] phoneNo;
-    
-    public ConvertJsonToJava(Map<String, Object> map) {
-            this.age=(int)map.get(age);
-            this.name=String.valueOf(map.get(name));
-            this.phoneNo=new Array(map.get(phoneNo));
+    private List<String> phoneNo;
 
+    public ConvertJsonToJava(Map<String, Object> map) throws JSONException {
+        this.age = (int) map.get("age");
+        this.name = String.valueOf(map.get("name"));
+
+        Object phoneNoObj = map.get("phoneNo");
+        if (phoneNoObj instanceof JSONArray) {
+            JSONArray phoneNoArray = (JSONArray) phoneNoObj;
+            this.phoneNo = new ArrayList<>();
+            for (int i = 0; i < phoneNoArray.length(); i++) {
+                this.phoneNo.add(phoneNoArray.getString(i));
+            }
+        }
     }
 
     public static Map<String, Object> convertJsonToMap(String json) throws JSONException {
@@ -38,53 +46,6 @@ public class ConvertJsonToJava {
         return result;
     }
 
-    
-
-    public static void main(String[] args) throws JSONException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-        String json = "{\"name\":\"Paul\",\"age\":20,\"phoneNo\":[\"12345\",\"98765\",\"567890\"]}";
-        // String json2 = "{\"name\": \"John Doe\", \"age\": 30, \"phoneNo\":\"0923456664\"}";
-        Map<String, Object> data = convertJsonToMap(json);
-        // ConvertJsonToJava obj1 = new ConvertJsonToJava(data);
-
-        Class Al=ConvertJsonToJava.class;
-        Field[] fields = Al.getDeclaredFields();
-        
-        
-        Map<String, String> fieldMap = new HashMap<>();
-        Map<String,Object>m1=new HashMap<>();
- 
-
-         for (Field field : fields) {
-             String fieldName = field.getName();
-             String fieldType = field.getType().getSimpleName();
-             System.out.println(fieldName +" ... "+fieldType);
-             fieldMap.put(fieldName, fieldType);
-         }
-         for(String i:fieldMap.keySet()){
-            String val1=fieldMap.get(i);
-            String returndata=getPrimitiveTypeName(data.get(i).getClass().getSimpleName());
-            System.out.println(data.get(i).getClass().getSimpleName());
-            System.out.println(returndata);
-            
-            
-             System.out.println(val1);
-            // if(data.get(i).getClass().getSimpleName().equals("JSONArray")){
-            //     // String arr[]=data.get(i).split("");
-
-            // }
-            if(val1.equals(returndata) ){
-                System.out.println("true");
-                m1.put(i,data.get(i));
-             }
-
-         }
-        System.out.println(m1);
-         
-        
-        //  ConvertJsonToJava obj= new ConvertJsonToJava();
-        }
-
-        
     public static String getPrimitiveTypeName(String wrapperClassName) {
         switch (wrapperClassName) {
             case "Integer":
@@ -93,11 +54,40 @@ public class ConvertJsonToJava {
                 return "String[]";
             case "String":
                 return "String";
-            // Add more mappings for other primitive types...
             default:
                 return null;
         }
     }
 
-    
+    public static void main(String[] args) throws JSONException, NoSuchFieldException, SecurityException,
+            IllegalArgumentException, IllegalAccessException {
+        String json = "{\"name\":\"Paul\",\"age\":20,\"phoneNo\":[\"12345\",\"98765\",\"567890\"]}";
+        // String json2 = "{\"name\": \"John Doe\", \"age\":
+        // 30,\"phoneNo\":\"0923456664\"}";
+        Map<String, Object> data = convertJsonToMap(json);
+
+        Class<?> Al = ConvertJsonToJava.class;
+        Field[] fields = Al.getDeclaredFields();
+
+        Map<String, String> fieldMap = new HashMap<>();
+        Map<String, Object> m1 = new HashMap<>();
+
+        for (Field field : fields) {
+            String fieldName = field.getName();
+            String fieldType = field.getType().getSimpleName();
+            fieldMap.put(fieldName, fieldType);
+        }
+        for (String i : fieldMap.keySet()) {
+            String val1 = fieldMap.get(i);
+            String returndata = getPrimitiveTypeName(data.get(i).getClass().getSimpleName());
+            if (val1.equals(returndata)) {
+                m1.put(i, data.get(i));
+            } else {
+                m1.put(i, "");
+            }
+        }
+
+        ConvertJsonToJava obj = new ConvertJsonToJava(data);
+        System.out.println("Name: " + obj.name + "\nAge: " + obj.age + "\nPhoneNumber: " + obj.phoneNo);
+    }
 }
