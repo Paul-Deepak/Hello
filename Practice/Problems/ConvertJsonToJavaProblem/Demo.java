949bce9d614da4e0b3c630eaa9db46a25fb3e706
Paul-Deepak <pauldeepak2003@gmail.com>
2023-11-21 15:10:15 +0530

    task update

---
+++ Practice/Problems/ConvertJsonToJavaProblem/Demo.java
@@ -0,0 +1,10 @@
+package Practice.Problems.ConvertJsonToJavaProblem;
+import java.util.List;
+public class Demo {
+    String name;
+    int age;
+     List<String> phoneNo;
+
+   
+    }
+
--- Practice/Problems/ConvertJsonToJavaProblem/Main.java
+++ Practice/Problems/ConvertJsonToJavaProblem/Main.java
@@ -1,16 +1,39 @@
 package Practice.Problems.ConvertJsonToJavaProblem;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.json.JSONObject;
+import java.lang.reflect.InvocationTargetException;
+
+import org.json.JSONException;
 
 public class Main {
-    public static void main(String args[]){
-    Map<String, String> map = new HashMap<>();
-map.put("name", "jon doe");
-map.put("age", "22");
-map.put("city", "chicago");
-JSONObject jo = new JSONObject(map);
-System.out.println(jo);
+    public static void main(String args[]) {
+        // String json2 = "{\"name\": \"John Doe\", \"rollNo\": 10,\"phoneNo\":[\"12345\",\"98765\",\"567890\"], \"age\": 30}";
+        // String json = "{\"name\": \"John\", \"age\":20}";
+        String json = "{\"name\":\"Paul\",\"age\":20,\"phoneNo\":[\"12345\",\"98765\",\"567890\"]}";
+        try {
+            Demo obj = ConvertJsonToJava.convertJsonToObj(json, Demo.class);
+            System.out.println(obj.name + "  " + obj.age + "  " + obj.phoneNo);
+        } catch (InstantiationException e) {
+
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+
+            e.printStackTrace();
+        } catch (SecurityException e) {
+
+            e.printStackTrace();
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+
+            e.printStackTrace();
+        }
     }
+
 }
